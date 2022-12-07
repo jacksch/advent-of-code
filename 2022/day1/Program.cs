612f948f0027dc06bd3559c719f5727e837b1da0
Jack Schofield <17806331+JackSch@users.noreply.github.com>
2022-12-08 10:59:56 +1100

    Add 2022 Day 1 solution, again

---
+++ 2022/day1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+string[] text = File.ReadAllLines(@".\input.txt");
+
+// Part 1: Get the elf with the most calories
+int currentTotal = 0;
+List<int> totals = new();
+
+for (int i = 0; i < text.Count(); i++)
+{
+    if (text[i] != "")
+    {
+        currentTotal += Convert.ToInt32(text[i]);
+    }
+    else if (text[i] == "" || i == text.Count() -1)
+    {
+        totals.Add(currentTotal);
+        currentTotal = 0;
+    }
+}
+
+// Part 2: Get the total calories for the top 3 elves
+int topThreeTotal = 0;
+
+var topThree = (from i in totals
+                orderby i descending
+                select i).Take(3);
+
+foreach (var x in topThree)
+{
+    topThreeTotal += x;
+}
+
+Console.WriteLine("The elf with the most calories of food has: {0}",totals.Max());
+Console.WriteLine("The top three elves has a total calories of: {0}",topThreeTotal);
