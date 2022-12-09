599cace2efe34a788354241f187c8c42c716f559
Jack Schofield <17806331+JackSch@users.noreply.github.com>
2022-12-09 15:46:44 +1100

    Add 2022 Day 6 solution

---
+++ 2022/day6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = File.ReadAllLines(@".\input.txt");
+        string message = input[0];
+
+        Console.WriteLine(GetResult(message, 4));
+        Console.WriteLine(GetResult(message, 14));
+    }
+    static string GetResult(string message, int num)
+    {
+        for (int i = 0; i < message.Length - (num - 1); i++)
+        {
+            var charArray = message.Substring(i, num).ToCharArray();
+            var result = charArray.Distinct();
+
+            if (result.Count() < num) continue;
+
+            return (num + i).ToString();
+        }
+        return "No result";
+    }
+}
