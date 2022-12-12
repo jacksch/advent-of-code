9a93fd9ee4e02b52b4b79070d347d8dfffb736e6
Jack Schofield <17806331+JackSch@users.noreply.github.com>
2022-12-12 15:33:52 +1100

    Add 2022 Day 10 solution

---
+++ 2022/day10/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+
+    public static int x = 1;
+    public static int n = 0;
+    public static int xPixel = 0;
+    public static int[] checks = { 20, 60, 100, 140, 180, 220 };
+    public static int cycleProductSum = 0;
+    static void Main()
+    {
+        string[] input = File.ReadAllLines(@"./input.txt");
+
+        Console.WriteLine("Part Two Solution:");
+
+        for (int i = 0; i < input.Count(); i++)
+        {
+            string[] instruction = input[i].Split(" ");
+
+            ExecuteInstruction(instruction);
+        }
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Part One Solution: {0}", cycleProductSum);
+    }
+    static void ExecuteInstruction(string[] instruction)
+    {
+        if (instruction[0] == "noop")
+        {
+            n++;
+
+            DrawPixel();
+            CycleCheck();
+        }
+        if (instruction[0] == "addx")
+        {
+            n++;
+
+            DrawPixel();
+            CycleCheck();
+
+            n++;
+            
+            DrawPixel();
+            CycleCheck();
+
+            x += int.Parse(instruction[1]);               
+        }
+    }
+    static void DrawPixel()
+    {      
+        if (xPixel == 39)
+        {
+            if (x == xPixel || x + 1 == xPixel || x - 1 == xPixel)
+                Console.WriteLine("#");
+            else
+                Console.WriteLine(" ");
+
+            xPixel = 0;
+        }
+        else
+        {
+            if (x == xPixel || x + 1 == xPixel || x - 1 == xPixel)
+                Console.Write("#");
+            else
+                Console.Write(" ");
+
+            xPixel++;
+        }
+    }
+    static bool CycleCheck()
+    {
+        var check = Array.Exists(checks, x => x == n);
+
+        if (check) cycleProductSum += n * x;
+
+        return check;
+    }
+}
--- 2022/day9/Program.cs
+++ 2022/day9/Program.cs
@@ -2,25 +2,46 @@
 using System.Text;
 using System.IO;
 
-class Program
+class Program2
 {
     public static List<string> tPositions = new();
+    public static List<string> tPositions2 = new();
     static void Main()
     {
-        string[] input = File.ReadAllLines(@"./input.txt");
+        string[] input = File.ReadAllLines(@"./sampleinput.txt");
 
         List<string[]> motions = ParseMotions(input);
         int[] pos = { 0, 0, 0, 0 };
+        int[,] knotPos = new int[10,2];
 
+        int i = 1; // Testing
         foreach (var motion in motions)
         {
             pos = MoveRope(motion, pos[0], pos[1], pos[2], pos[3]);
+
+            knotPos = MoveLongRope(motion, knotPos);
+            // foreach (var knot in knotPos)
+            // {
+            //     Console.WriteLine("{0}",knot);//[0],knot[1]);
+            // }
+
+
+            // Console.WriteLine("Head: {0},{1} Tail: {2},{3}", pos[0], pos[1], pos[2], pos[3]);
+            // Console.WriteLine("Move {0} complete", i);
+            // Console.WriteLine("");
+
+            // if (i == 2) break;
+            i++;
         }        
 
         int tPositionsCount = (from x in tPositions
                                select x).Distinct().Count();
 
+        int tPositions2Count = (from x in tPositions2
+                               select x).Distinct().Count();
+
         Console.WriteLine("Part One Solution: {0}", tPositionsCount);
+        // Console.WriteLine("Part Two Solution: {0}", tPositions2Count);
     }
 
     static List<string[]> ParseMotions(string[] input)
@@ -34,6 +55,25 @@ class Program
         }
 
         return motions;
+    }
+    static int[,] MoveLongRope(string[] motion, int[,] pos)
+    {
+        int distance = Convert.ToInt32(motion[1]);
+        int[] newKnotPos = {0,0};
+        // Use floats?
+
+        for (int i = 0; i < distance; i++)
+        {
+            int[] newLeadPos = MoveHead(motion, pos[0, 0], pos[0, 1]);
+
+            // Simulate a rope?
+
+
+
+            // tPositions2.Add($@"{pos[^0,0].ToString()},{pos[^0,1].ToString()}");
+        }
+
+        return pos;
     }
     static int[] MoveRope(string[] motion, int headX, int headY, int tailX, int tailY)
     {
