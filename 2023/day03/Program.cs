19a876f997c0276412c577a0830737b71e5456aa
Jack Schofield <17806331+JackSch@users.noreply.github.com>
2023-12-03 22:11:39 +1100

    Add Day 3 WIP/boilerplate + update README

---
+++ 2023/day03/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace day03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = File.ReadAllLines(@"./sampleinput.txt");
+        // string[] input = File.ReadAllLines(@"./input.txt");
+
+        int partOneResult = PartOne(input);
+        // int partTwoResult = PartTwo(input);
+
+        Console.WriteLine("Part One Solution: {0}", partOneResult);
+        // Console.WriteLine("Part Two Solution: {0}", partTwoResult);
+    }
+
+    static int PartOne(string[] input)
+    {
+
+
+        return 0;
+    }
+}
