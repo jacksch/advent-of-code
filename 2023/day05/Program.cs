ce4e1430069e6b11d90c3b74d9a903d694439d56
Jack Schofield <17806331+JackSch@users.noreply.github.com>
2023-12-11 17:07:35 +1100

    Add Day 5 Part 1 solution

--- 2023/day05/Program.cs
+++ 2023/day05/Program.cs
@@ -1,19 +1,22 @@
-﻿namespace day05;
+﻿using System.Data;
+using System.Runtime.InteropServices;
+
+namespace day05;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string[] input = File.ReadAllLines(@"./sampleinput.txt");
-        // string[] input = File.ReadAllLines(@"./input.txt");
+        // string[] input = File.ReadAllLines(@"./sampleinput.txt");
+        string[] input = File.ReadAllLines(@"./input.txt");
 
         Solution solution = new(input);
 
-        int partOneResult = solution.PartOne();
-        int partTwoResult = solution.PartTwo();
+        uint partOneResult = solution.PartOne();
+        // uint partTwoResult = solution.PartTwo();
 
         Console.WriteLine("Part One Solution: {0}", partOneResult);
-        Console.WriteLine("Part Two Solution: {0}", partTwoResult);
+        // Console.WriteLine("Part Two Solution: {0}", partTwoResult);
     }
 }
 
@@ -21,64 +24,101 @@ public class Solution
 {
     private readonly string[] _input;
     private readonly uint[] _seeds;
+
+    private readonly List<List<uint[]>> _maps;
+
     public Solution(string[] input)
     {
         _input = input;
         _seeds = ParseSeeds();
+        _maps = ParseMaps();
     }
 
-    public int PartOne()
+    public uint PartOne()
     {
-        foreach (uint seed in _seeds)
+        List<uint> locations = [];
+        foreach (uint currentSeed in _seeds)
         {
+            uint seed = currentSeed;
+            bool breakLine = false;
             for (int i = 2; i < _input.Length; i++)
             {
-                if (seed != 79) continue; // Testing
-                int m = 0;
                 string[] line = _input[i].Split(" ");
-                if (String.IsNullOrWhiteSpace(_input[i]) && m == 2)
-                {
-                    break; // Testing
-                    m++;
-                }
+
                 if (String.IsNullOrWhiteSpace(_input[i]))
                 {
-                    Console.WriteLine(_input[i + 1]);
+                    breakLine = false;
                     continue;
                 }
-                
 
-                if (line.Length == 2)
+                if (line.Length == 2 || breakLine)
                     continue;
                 
                 uint destIdx = uint.Parse(line[0]);
                 uint srcIdx = uint.Parse(line[1]);
                 uint range = uint.Parse(line[2]);
 
-                Console.WriteLine("Seed: {0}, Dest: {1}, Src: {2}, Range {3}", seed, destIdx, srcIdx, range);
-
-                // if src - range - 1 == seed, etc
-                if (srcIdx < seed && srcIdx + range - 1 > seed)
+                if (srcIdx < seed && srcIdx + range > seed)
                 {
-                    // Console.WriteLine("We have something?");
-                    long offset = (long)destIdx - (long)srcIdx;
-                    long newSeed = seed + offset;
-                    Console.WriteLine(newSeed);
-                }
-                else
-                {
-                    // Should stay the same?
-                }
+                    if (srcIdx < destIdx)
+                        seed += destIdx - srcIdx;
+                    else
+                        seed -= srcIdx - destIdx;
 
+                    breakLine = true;
+                }
             }
+            locations.Add(seed);
         }
-        return 0;
+
+        return locations.Min();
     }
+
+    // public uint PartTwo()
+    // {
+    //     for (int i = 0; i < _seeds.Length; i += 2)
+    //     {
+    //         uint seedNumber = _seeds[i];
+    //         uint seedRange = _seeds[i + 1];
+
+
+
+
+    //     }
+
+    //     return 0;
+    // }
+
+
+    public List<List<uint[]>> ParseMaps()
+    {
+        List<List<uint[]>> mapList = [];
+        List<uint[]> map = [];
+        for (int i = 2; i < _input.Length; i++)
+        {
+            string[] line = _input[i].Split(" ");
+
+            if (String.IsNullOrWhiteSpace(_input[i]) || line.Length == 2)
+            {
+                if (map.Count > 0)
+                    mapList.Add(map);
+
+                map = [];
+                continue;
+            }
+
+            uint destIdx = uint.Parse(line[0]);
+            uint srcIdx = uint.Parse(line[1]);
+            uint range = uint.Parse(line[2]);
+
+            map.Add([destIdx, srcIdx, range]);
+        }
+        mapList.Add(map);
+
+        return mapList;
+    }
+
 
     public uint[] ParseSeeds() =>
         _input[0].Split(": ")[1].Split(" ").Select(uint.Parse).ToArray();
-
-    public int PartTwo() =>
-        0;
-    
 }
